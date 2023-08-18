bb2181d4a3bcf2ba9120cdf6d008c0bafea25d47
Mostafa Essam <57865081+M0stu@users.noreply.github.com>
2023-08-18 16:47:07 +0300

    Telegram Project added missing .env file

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+const app = require("./app");
+
+dotenv.config({ path: "./.env" });
+
+// Connect database
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Database connected..."))
+  .catch((error) => console.log("An error occured..."));
+
+// Serve client folder
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+PORT=process.env.API_PORT || 4000;
+
+//   Listen to port
+exports.expressServer = app.listen(PORT, () =>
+  console.log(`Listening on ${PORT}`)
+);
